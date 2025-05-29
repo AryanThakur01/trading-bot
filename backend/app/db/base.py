cfa3b744e34f23d36308b0bc9d3510302c622a31
AryanThakur01 <aryan197297@gmail.com>
2025-05-29 15:11:21 +0530

    feat: formatters and db base model

--- backend/app/db/base.py
+++ backend/app/db/base.py
@@ -1,11 +1,21 @@
-from sqlmodel import create_engine, Session
 from app.settings import settings
+from sqlmodel.ext.asyncio.session import AsyncSession
+from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.orm import sessionmaker
 
-DATABASE_URI = settings.DATABASE_URI
+DATABASE_URL = settings.DATABASE_URI.replace(
+    "postgresql://", "postgresql+asyncpg://")
 
-engine = create_engine(DATABASE_URI, echo=True)
+engine = create_async_engine(DATABASE_URL, echo=True)
+
+# Async session factory
+async_session = sessionmaker(
+    engine, class_=AsyncSession, expire_on_commit=False
+)
+
+# Dependency for FastAPI to get async session
 
 
-def get_session():
-    with Session(engine) as session:
+async def get_session():
+    async with async_session() as session:
         yield session
